45f7780953080294fcb0448bcf109edbdadecf9e
Ibrahim Wassouf <ib981347@dal.ca>
2023-05-25 08:57:10 -0300

    Add specific target file formats for prettier in null-ls

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -5,41 +5,53 @@ return {
 	},
 	config = function()
 		local null_ls = require("null-ls")
-		local group = vim.api.nvim_create_augroup("lsp_format_on_save", {clear = false })
+		local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
 		local event = "BufWritePre"
 		local async = event == "BufWritePost"
 
 		--setup from MunifTanjim/prettier.nvim.git
 		null_ls.setup({
 			on_attach = function(client, bufnr)
-					if client.supports_method("textDocument/formatting") then
-						vim.keymap.set("n", "<Leader>f", function()
-							vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-						end, { buffer = bufnr, desc = "[lsp] format" })
+				if client.supports_method("textDocument/formatting") then
+					vim.keymap.set("n", "<Leader>f", function()
+						vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+					end, { buffer = bufnr, desc = "[lsp] format" })
 
-						-- format on save
-						vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-						vim.api.nvim_create_autocmd(event, {
-							buffer = bufnr,
-							group = group,
-							callback = function()
-								vim.lsp.buf.format({ bufnr = bufnr, async = async })
-							end,
-							desc = "[lsp] format on save",
-						})
-					end
+					-- format on save
+					vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+					vim.api.nvim_create_autocmd(event, {
+						buffer = bufnr,
+						group = group,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = bufnr, async = async })
+						end,
+						desc = "[lsp] format on save",
+					})
+				end
 
-					if client.supports_method("textDocument/rangeFormatting") then
-						vim.keymap.set("x", "<Leader>f", function()
-							vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-						end, { buffer = bufnr, desc = "[lsp] format" })
-					end
-				end,
+				if client.supports_method("textDocument/rangeFormatting") then
+					vim.keymap.set("x", "<Leader>f", function()
+						vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+					end, { buffer = bufnr, desc = "[lsp] format" })
+				end
+			end,
 
 			--sources I put myself may be unneeded
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.prettier.with({
+					"javascript",
+					"typescript",
+					"css",
+					"scss",
+					"html",
+					"json",
+					"markdown",
+					"md",
+					"txt",
+					"graphql",
+					"yaml",
+				}),
 				null_ls.builtins.diagnostics.eslint_d,
 			},
 		})
