0b33c7f1df69a288a526777b088b8e620d972432
Ibrahim Wassouf <ib981347@dal.ca>
2023-07-09 16:03:52 -0300

    Replace goto definition keymap binding
    Used the builtin lsp.vim.buf command because
    LspSaga was not adding tag to the tagstack
    preventing from going back to the original file

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,11 +2,12 @@ vim.o.number = true
 vim.o.autoindent = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
-vim.o.syntax = "on"
+--vim.o.syntax = "on"
 vim.o.expandtab = true
 vim.o.linebreak = true
 vim.o.textwidth = 72
 vim.o.wrap = false
+vim.o.tagstack = true
 
 vim.g.mapleader = " "
 
@@ -20,14 +21,14 @@ vim.keymap.set("n", "<leader>gw", "gwvip")
 
 --lsp-saga
 vim.keymap.set("n", "gp", "<cmd>Lspsaga peek_definition<CR>")
-vim.keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<CR>")
+vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
 vim.keymap.set("n", "pt", "<cmd>Lspsaga peek_type_definition<CR>")
 vim.keymap.set("n", "gh", "<cmd>Lspsaga lsp_finder<CR>")
 vim.keymap.set("n", "ld", "<cmd>Lspsaga show_line_diagnostics<CR>")
 vim.keymap.set("n", "<leader>K", ":Lspsaga hover_doc<CR>")
 vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<cr>")
-
+vim.keymap.set("n", "<leader>gr", "<cmd>Lspsaga rename<CR>")
 vim.keymap.set("n", "<leader>vd", "<cmd>vim.diagnostic.open_float<cr>")
-
+vim.keymap.set("n", "<leader>bd", "<cmd>Lspsaga show_buf_diagnostics<CR>")
 --trouble keymaps
 vim.keymap.set("n", "<leader>xx", "<cmd>silent! TroubleToggle<cr>", { silent = true, noremap = true })
