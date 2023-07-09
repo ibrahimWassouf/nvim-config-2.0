4025d57cbdfbeaea0ba0f89be1079273d0f3f208
Ibrahim Wassouf <ib981347@dal.ca>
2023-07-09 16:03:00 -0300

    Add commented rome server for future

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -4,50 +4,51 @@
 --This should make it sync across Mason as well
 
 return {
-  {
-    "williamboman/mason.nvim",
-    build = ":MasonUpdate",
-    dependencies = {
-      "neovim/nvim-lspconfig",
-    },
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup()
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({})
-      lspconfig.lua_ls.setup({})
-      lspconfig.hls.setup({})
-      lspconfig.ltex.setup({
-        settings = {
-          ltex = {
-            language = "en-CA",
-          },
-        },
-      })
-    end,
-  },
-  {
-    "glepnir/lspsaga.nvim",
-    event = "LspAttach",
-    dependencies = {
-      { "nvim-tree/nvim-web-devicons" },
-      --Please make sure you install markdown and markdown_inline parser
-      { "nvim-treesitter/nvim-treesitter" },
-    },
-    config = function()
-      require("lspsaga").setup({
-        debug = true,
-      })
-    end,
-  },
+	{
+		"williamboman/mason.nvim",
+		build = ":MasonUpdate",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup()
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			--lspconfig.rome.setup({})
+			lspconfig.tsserver.setup({})
+			lspconfig.lua_ls.setup({})
+			lspconfig.hls.setup({})
+			lspconfig.ltex.setup({
+				settings = {
+					ltex = {
+						language = "en-CA",
+					},
+				},
+			})
+		end,
+	},
+	{
+		"glepnir/lspsaga.nvim",
+		event = "LspAttach",
+		dependencies = {
+			{ "nvim-tree/nvim-web-devicons" },
+			--Please make sure you install markdown and markdown_inline parser
+			{ "nvim-treesitter/nvim-treesitter" },
+		},
+		config = function()
+			require("lspsaga").setup({
+				debug = true,
+			})
+		end,
+	},
 }
