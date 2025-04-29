79bfeaa9fed8d14a9e33d2ff57df07cf1b67dce2
Ibrahim Wassouf <ib981347@dal.ca>
2025-04-29 19:02:15 -0300

    Update to neovim 0.11 & remove uneeded plugins

--- init.lua
+++ init.lua
@@ -19,3 +19,18 @@ if vim.g.neovide then
   vim.g.neovide_window_blurred = true
   vim.g.neovide_cursor_animation_length = 0
 end
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+  callback = function(args)
+    -- 2
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      -- 3
+      buffer = args.buf,
+      callback = function()
+        -- 4 + 5
+        vim.lsp.buf.format {async = false, id = args.data.client_id }
+      end,
+    })
+  end
+})
